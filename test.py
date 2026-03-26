e8aab85272e8df43e128c75bd50b8f7b1884f62a
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-03-27 04:27:52 +0800

    removed files

--- test.py
+++ test.py
@@ -2,8 +2,41 @@ from database_connector.base import DatabaseConnector
 from database_connector.sqlite_connector import SQLiteConnector
 from database_connector.connection_pool import ConnectionPool
 
-with SQLiteConnector('student_db') as conn:
-    conn.execute('''CREATE TABLE Students(
+#with SQLiteConnector('student_db') as conn:
+ #   conn.execute('''CREATE TABLE Students(
+  #     name TEXT NOT NULL,
+   #     age INTEGER NOT NULL,
+    #    email TEXT NOT NULL,
+     #   course TEXT NOT NULL
+    #);''')
+    #print("Table created.")
+    #conn.execute("INSERT INTO Students (name, age, email, course) VALUES (?,?,?,?)",
+     #            ('Thiyane Xavier', 21, 'thiyane@gmail.com', 'Information Technology'))
+    #conn.execute("INSERT INTO Students (name,age, email, course) VALUES (?,?,?,?)", 
+    #             ('Mayra Tivane', 21, 'mayrativane2015@gmail.com', 'Contabilidade e Auditoria'))
+    #conn.execute("INSERT INTO Students (name,age,email,course) VALUES (?,?,?,?)",
+     #            ('Bruna Tivane', 19, 'brunativane2015@gmail.com', 'Computer Science'))
+    
+    #print("Student inserted.")
+    #results = conn.execute("SELECT * FROM Students")
+    #results1 = conn.execute("SELECT * FROM Students WHERE age == 21")
+    #print(results)
+    #print("\n", results1)
+    
+#connect = ConnectionPool(SQLiteConnector,3, db_path = 'student_db')
+
+#conn1=connect.get_connection()
+#conn2=connect.get_connection()
+#conn3=connect.get_connection()
+#try:
+#    conn4 = connect.get_connection()
+#except RuntimeError as e:
+#    print(e)
+#conn5 = connect.release_connection(conn3)
+#conn6 = connect.close_all()
+
+with SQLiteConnector('guest_db1') as conn:
+    conn.execute('''CREATE TABLE IF NOT EXISTS Guests1(
         std_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         age INTEGER NOT NULL,
@@ -11,27 +44,43 @@ with SQLiteConnector('student_db') as conn:
         course TEXT NOT NULL
     );''')
     print("Table created.")
-    conn.execute("INSERT INTO Students (name, age, email, course) VALUES (?,?,?,?)",
-                 ('Thiyane Xavier', 21, 'thiyane@gmail.com', 'Information Technology'))
-    conn.execute("INSERT INTO Students (name,age, email, course) VALUES (?,?,?,?)", 
-                 ('Mayra Tivane', 21, 'mayrativane2015@gmail.com', 'Contabilidade e Auditoria'))
-    conn.execute("INSERT INTO Students (name,age,email,course) VALUES (?,?,?,?)",
-                 ('Bruna Tivane', 19, 'brunativane2015@gmail.com', 'Computer Science'))
+    conn.execute("INSERT INTO Guests1 (name, age, email, course) VALUES (?,?,?,?)",
+                 ('Thiyane Xavier', 21, 'thiyane@gmail.com', 'Noivo'))
+    print("\n")
+    conn.execute("INSERT INTO Guests1 (name,age, email, course) VALUES (?,?,?,?)", 
+                 ('Mayra Tivane', 21, 'mayrativane2015@gmail.com', 'Noiva'))
+    print("\n")
+    conn.execute("INSERT INTO Guests1 (name,age,email,course) VALUES (?,?,?,?)",
+                 ('Bruna Tivane', 19, 'brunativane2015@gmail.com', 'Irma da Noiva'))
+    print("\n")
+    conn.execute("INSERT INTO Guests1 (name,age,email,course) VALUES (?,?,?,?)",
+                 ('Patricia Vilankulo', 28, 'patricia@gmail.com', 'Irma da Noiva'))
+    print("\n")
+    conn.execute("INSERT INTO Guests1 (name,age,email,course) VALUES (?,?,?,?)",
+                 ('Maria Tivane', 52, 'maria@gmail.com', 'Mae da Noiva'))
+    print("\n")
+    conn.execute("INSERT INTO Guests1 (name,age,email,course) VALUES (?,?,?,?)",
+                 ('Neusa Torres', 47, 'Neusa@gmail.com', 'Mae da Noivo'))
+    print("\n")
     
-    print("Student inserted.")
-    results = conn.execute("SELECT * FROM Students")
-    results1 = conn.execute("SELECT * FROM Students WHERE age == 21")
+    print('Guests1 inserted.')
+    results = conn.execute("SELECT * FROM Guests1")
+    results1 = conn.execute("SELECT * FROM Guests1 WHERE age == 21")
     print(results)
+    print("\n")
     print("\n", results1)
     
-connect = ConnectionPool(SQLiteConnector,3, db_path = 'student_db')
-
-conn1=connect.get_connection()
-conn2=connect.get_connection()
-conn3=connect.get_connection()
-try:
-    conn4 = connect.get_connection()
-except RuntimeError as e:
-    print(e)
-conn5 = connect.release_connection(conn3)
-conn6 = connect.close_all()
+    
+   
+    
+    connect1 = ConnectionPool(SQLiteConnector, 3, db_path='guests_db1')
+    conn1 = connect1.get_connection()
+    conn2 = connect1.get_connection()
+    conn3 = connect1.get_connection()
+    try:
+        conn4 = connect1.get_connection()
+    except RuntimeError as e:
+        print(e)
+    conn5 = connect1.release_connection(conn3)
+    conn6 = connect1.close_all()
+    
